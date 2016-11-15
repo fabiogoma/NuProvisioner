fdd0f605f92f7bcb1d49e2f0ab24fca3d908ba4e
Fabio G. Martins <fabiogoma@gmail.com>
2016-11-15 11:55:58 -0200

    SQS implemented

--- src/main/java/br/com/nubank/Provisioner.java
+++ src/main/java/br/com/nubank/Provisioner.java
@@ -1,11 +1,10 @@
 package br.com.nubank;
 
 import java.text.ParseException;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +14,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.nubank.helpers.ScheduleParser;
-import br.com.nubank.pojos.Scheduler;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+
 
 @RestController
 @EnableAutoConfiguration
@@ -25,8 +28,18 @@ public class Provisioner {
     @RequestMapping(value = "/schedule/", method = RequestMethod.POST)
     @ResponseBody
     public void schedule(@RequestBody Map<String, Object> payload) throws JSONException, ParseException{
-    	Scheduler sched = ScheduleParser.JsonToObject(payload);
+
+    	JSONObject jsonObject = new JSONObject(payload);
+    	String scheduler = jsonObject.toString();
     	
+    	AWSCredentials credentials = new ProfileCredentialsProvider().getCredentials();
+		AmazonSQS sqs = new AmazonSQSClient(credentials);
+		
+		System.out.println("Sending message");
+		sqs.sendMessage(new SendMessageRequest("https://us-west-2.queue.amazonaws.com/678982507510/sqs_launch", scheduler));
+    	
+		/*
+    	Scheduler sched = ScheduleParser.JsonToObject(payload);    	
     	System.out.println(sched.getImage());
     	System.out.println(sched.getSchedule());
     	
@@ -36,7 +49,7 @@ public class Provisioner {
 		{
 		    System.out.println(entry.getKey() + "=" + entry.getValue());
 		}
-   
+		*/
     }
 
     @RequestMapping("/list")
